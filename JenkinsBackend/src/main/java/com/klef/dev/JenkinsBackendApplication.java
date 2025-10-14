f7632f121d103d165d045f2a7e457fdaf5ea5edc
Hemanth-Moka <2300030439@kluniversity.in>
2025-10-14 22:42:19 +0530

    v1

---
+++ JenkinsBackend/src/main/java/com/klef/dev/JenkinsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.klef.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsBackendApplication.class, args);
+	}
+
+}
